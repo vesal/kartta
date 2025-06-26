7034156bc67b6a75d961ceca35331154bae2d9de
Vesa Lappalainen <vesal@jyu.fi>
2025-06-26 17:38:08 +0300

    igc curl options

--- igc.php
+++ igc.php
@@ -16,9 +16,14 @@ function http_get($url) {
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_HEADER, false);
     curl_setopt($curl, CURLOPT_POST, false);
-	curl_setopt($curl, CURLOPT_REFERER, "https://www.xcontest.org");
+	  curl_setopt($curl, CURLOPT_REFERER, "https://www.xcontest.org");
+	  curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true); // seuraa uudelleenohjauksia
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); // palauta data, ei tulosta
     //curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
     $json_response = curl_exec($curl);
+    if ($json_response === false) {
+        die("CURL ERROR: " . curl_error($curl));
+    }
     $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
     if ( $status != 201 && $status != 200) {
         die("ERROR: call to URL $url failed with status $status");
