c68c0f7e30ee59e1a88290e34533a1bc2a222f17
Vesa Lappalainen <vesal@jyu.fi>
2025-08-21 16:25:48 +0300

    MapWrapper.js

--- offline-worker.js
+++ offline-worker.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = "k3-cache-v2";
+const CACHE_NAME = "k3-cache-v3";
 const FILES_TO_CACHE = [
   "./k3.html",
   "./js/leaflet.js",
   "./js/leaflet.textpath.js",
   "./js/leaflet.css",
+  "./MapWrapperLeafLet.js",
   "./manifest.json",
   "./offline-worker.js",
 ];
